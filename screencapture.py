4c43201ba1427eb3267bcfebc6132f0f9998a4bc
swapnil londhe <37972263+swapnillondhe24@users.noreply.github.com>
2020-12-16 13:59:27 +0530

    Update screencapture.py

--- screencapture.py
+++ screencapture.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 	for i in range(0,10):
 		images_folder = "orig_images/"+str(i)+"/"
 		number = images_folder.split("/")[1]
-		for i in range (45,50):
+		for i in range (0,50):
 			print("Draw "+ number +" within 5 seconds")
 			time.sleep(5)
 			im = ImageGrab.grab(bbox=(80, 160, 300, 380)) # X1,Y1,X2,Y2
